eeec6283eaa7a8ad79e9de9a6ca5f969f21b9301
sujittripathy <sujit.tripathy@icloud.com>
2017-03-22 20:50:18 -0700

    updated

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,22 +1,20 @@
 angular.module("weatherman-module",[])
 		.controller("MainController", function($scope,$http)
 			{
-			    $scope.onSubmit = function(valid){
-			        if(valid){
-			                            console.log("Form Submitted");
-			                            getWeather();
+				$scope.getWeather = function(valid){
+				    console.log(valid+","+$scope.weatherForm);
+                    if(valid){
+                        $http.get('/weather/v1/city/'+$scope.city+'/'+$scope.unit)
+                            .then(function(result,error){
+                                $scope.response = result.data;
+                                $scope.error = error;
+                         });
+                    }
+				}
 
-			        }else{
-			                            console.log("Invalid Form Submitted");
-
-			        }
-			    };
-				getWeather = function(){
-				console.log($scope.formModel);
-						$http.get('/weather/v1/city/'+$scope.formModel.city+'/'+$scope.formModel.unit)
-							.then(function(result,error){
-								$scope.response = result.data;
-								$scope.error = error;
-						 });
-				}					
-			});
+				$scope.reset = function(){
+                    $scope.response = null;
+                    $scope.city = null;
+                    $scope.unit = "imperial";
+				}
+		});
